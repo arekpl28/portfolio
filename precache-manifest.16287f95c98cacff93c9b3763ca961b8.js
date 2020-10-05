f2aca8918339f24140f46ad7586baa7f53763f88
Arkadiusz Plumbaum <arekpl28@wp.pl>
2020-10-05 13:08:44 +0200

    Updates

---
+++ precache-manifest.16287f95c98cacff93c9b3763ca961b8.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ffc7f535774bf03eb2ba84a09284059",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "4f710427228489482a24",
+    "url": "/portfolio/static/css/2.0b3cff39.chunk.css"
+  },
+  {
+    "revision": "4f710427228489482a24",
+    "url": "/portfolio/static/js/2.e0772553.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/portfolio/static/js/2.e0772553.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d03ee63519b8e4823b1",
+    "url": "/portfolio/static/js/main.764e64fc.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "2167fed1580f162e68099f448d1db74d",
+    "url": "/portfolio/static/media/cloud1.2167fed1.png"
+  },
+  {
+    "revision": "41d1e7792a2728b629cc6364de045279",
+    "url": "/portfolio/static/media/cloud2.41d1e779.png"
+  },
+  {
+    "revision": "7c878e086b8ba8d11993fd2cc1754379",
+    "url": "/portfolio/static/media/cloud3.7c878e08.png"
+  },
+  {
+    "revision": "ea046c5c1b3407aab18460846eafe93f",
+    "url": "/portfolio/static/media/cloud4.ea046c5c.png"
+  },
+  {
+    "revision": "34ae4c87d746dcf1f2e9fcb5fe049180",
+    "url": "/portfolio/static/media/cloud5.34ae4c87.png"
+  }
+]);
